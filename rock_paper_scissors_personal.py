706ce9488cab6f0f93be8f43e033379e80f9c25e
atanasraychev <raychev82@gmail.com>
2024-05-25 20:58:00 +0300

    Update rock_paper_scissors_personal.py

--- rock_paper_scissors_personal.py
+++ rock_paper_scissors_personal.py
@@ -1,44 +1,58 @@
+from colorama import Fore, Back, Style
 import random
 
-input_check = 1
+game_restart = "Yes"
 
-while input_check == 1:
-    player_message = int(input(f'Choose from the list :  Rock, Paper or Scissors! \n'
-                           '1 : Rock\n'
-                           '2 : Paper\n'
-                           '3 : Scissors\n'))
+while game_restart == 'Yes':
 
-    if player_message == 1:
-        player_choice = "Rock"
-        input_check = 0
-    elif player_message == 2:
-        player_choice = 'Paper'
-        input_check = 0
-    elif player_message == 3:
-        player_choice = "Scissors"
-        input_check = 0
-    else:
-        print("Wrong choice! Try again - 1, 2 or 3 :)\n")
+    input_check = 1
 
-computer_list = ["Rock", "Paper", "Scissors"]
+    while input_check == 1:
+        player_message = int(input(f'Choose from the list :  Rock, Paper or Scissors! \n'
+                               '1 : Rock\n'
+                               '2 : Paper\n'
+                               '3 : Scissors\n'))
 
-computer_choice = random.choice(computer_list)
+        if player_message == 1:
+            player_choice = "Rock"
+            input_check = 0
+        elif player_message == 2:
+            player_choice = 'Paper'
+            input_check = 0
+        elif player_message == 3:
+            player_choice = "Scissors"
+            input_check = 0
+        else:
+            print("Wrong choice! Try again - 1, 2 or 3 :)\n")
 
-print(f'Player\'s choice : {player_choice}')
-print(f'Computer\'s choice : {computer_choice}')
+    computer_list = ["Rock", "Paper", "Scissors"]
 
-if computer_choice == computer_list[0] and player_choice == "Paper":
-    print("Player wins")
-if computer_choice == computer_list[0] and player_choice == "Scissors":
-    print("Computer wins")
-elif computer_choice == computer_list[1] and player_choice == "Rock":
-    print("Computer wins")
-elif computer_choice == computer_list[1] and player_choice == "Scissors":
-    print("Player wins")
-elif computer_choice == computer_list[2] and player_choice == "Rock":
-    print("Player wins")
-elif computer_choice == computer_list[2] and player_choice == "Paper":
-    print("Computer wins")
-elif computer_choice == player_choice:
-    print("Draw")
+    computer_choice = random.choice(computer_list)
 
+    print(f'Player\'s choice : {player_choice}')
+    print(f'Computer\'s choice : {computer_choice}')
+
+    if computer_choice == computer_list[0] and player_choice == "Paper":
+        print(Fore.GREEN + "Player Wins")
+    if computer_choice == computer_list[0] and player_choice == "Scissors":
+        print(Fore.RED +"Computer wins")
+    elif computer_choice == computer_list[1] and player_choice == "Rock":
+        print(Fore.RED + "Computer wins")
+    elif computer_choice == computer_list[1] and player_choice == "Scissors":
+        print(Fore.GREEN + "Player wins")
+    elif computer_choice == computer_list[2] and player_choice == "Rock":
+        print(Fore.GREEN + "Player wins")
+    elif computer_choice == computer_list[2] and player_choice == "Paper":
+        print(Fore.RED + "Computer wins")
+    elif computer_choice == player_choice:
+        print(Fore.YELLOW + "Draw")
+
+    print(Style.RESET_ALL)
+    print("Do you want to play again? - Yes or No?")
+
+    game_restart = input()
+
+    if game_restart == 'No':
+        break
+
+print('Thanks for playing')
